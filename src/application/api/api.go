ef9e08b8fc629ed6661eb012b2cfaed71ac1b914
Guilherme Poglia <guilherme.poglia@edu.pucrs.br>
2024-09-26 18:40:21 -0300

    api and app setup

--- src/application/api/api.go
+++ src/application/api/api.go
@@ -1,1 +1,25 @@
 package api
+
+import (
+	"database/sql"
+	hello-world....
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Setup(router *gin.Engine, db *sql.DB) {
+
+
+	helloService := hello.NewService(db)
+
+
+	v1 := router.Group("/api/v1")
+
+	helloWorldController.Handler(v1, helloService)
+
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+}
