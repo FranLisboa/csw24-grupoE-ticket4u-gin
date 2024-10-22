eae28d7195826aa6b532e62b660892f8cbe81b81
David C <david.castro001@edu.pucrs.br>
2024-10-22 13:09:40 -0300

    terraform

--- src/main.tf
+++ src/main.tf
@@ -1,35 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+
 provider "aws" {
   region = "us-east-1"
+  shared_credentials_files = ["~/.aws/credentials"]
 }
 
-resource "aws_key" "key" {
-    key_name = "ec2-key"
-    public_key = file("~/.ssh/id_rsa.pub")
+resource "tls_private_key" "rsa-4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-resource "aws_instance" "t1-v1-ticket-api" {
-  ami           = "ami-007855ac798b5175e" # Ubuntu 22.04 LTS
-  instance_type = "t2.micro"
-  key_name =  aws_key.key.ke_name
-y
-  tags = {
-    Name = "t1-v1-ticket-api"
-  }
+variable key_name{}
 
-  vpc_security_group_ids = [aws_security_group.docker_sg.id]
-  
-  user_data = <<-EOF
-              #!/bin/bash
-              # primeiro atualizar pacotes e instalar o docker
-                sudo apt-get update -y
-                amazon-linux-extras install docker -y
-                service docker start
-                usermod -a -G docker ec2-user
-                chkconfig docker on
-                # baixar e rodar o container
-                docker run -d -p 80:80 --name ticket-api nginx
-                EOF
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa-4096.public_key_openssh
+}
 
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa-4096.private_key_pem
+  filename = var.key_name
 }
 
 resource "aws_security_group" "docker_sg" {
@@ -57,13 +56,12 @@ resource "aws_security_group" "docker_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-
- ingress {
+  ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
- }
+  }
 
   egress {
     from_port   = 0
@@ -71,4 +69,34 @@ resource "aws_security_group" "docker_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_instance" "t1_ticket_api" {
+  ami           = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.key_pair.key_name
+
+  tags = {
+    Name = "t1_ticket_api"
+  }
+
+  vpc_security_group_ids = [aws_security_group.docker_sg.id]
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              # Update package index
+              apt-get update -y
+              # Install Docker
+              apt-get install docker.io -y
+              # Start Docker service
+              systemctl start docker
+              systemctl enable docker
+              # Clone github
+              git clone https://github.com/GuilhermePoglia/csw24-grupoE-ticket4u-gin.git
+              # Entering repo
+              cd /home/ubuntu/csw24-grupoE-ticket4u-gin/src
+              # Build Docker image
+              docker compose up db -d
+              docker compose up app -d
+              EOF
 }
