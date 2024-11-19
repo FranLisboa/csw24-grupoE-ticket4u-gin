257c18d28b758a119d31da957dc31d328e7e446f
Francisco Lisboa <francisco.lisboa@poatek.com>
2024-11-19 00:39:05 -0300

    fix rds

--- src/infrastructure/database/connection.go
+++ src/infrastructure/database/connection.go
@@ -2,16 +2,14 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
+	//"fmt"
 	"log"
-	"time"
-	"os"
+	//"os"
 
 	_ "github.com/lib/pq"
 )
 
 func StartDB() *sql.DB {
- 	password :=  os.Getenv("DB_PASSWORD")
 
 	const (
 		host     = "postgres.cy3myhw5bsdp.us-east-1.rds.amazonaws.com"
@@ -20,16 +18,11 @@ func StartDB() *sql.DB {
 		dbname   = "postgres"
 	)
 
-	var psqlconn = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", psqlconn)
+	db, err := sql.Open("postgres", "postgresql://postgres:xyV4YBeY8Qz2FuZ@postgres.cy3myhw5bsdp.us-east-1.rds.amazonaws.com:5432/postgres")
 	if err != nil {
 		log.Fatalf("Erro ao abrir a conexão com o banco de dados: %v", err)
 	}
 
-	db.SetMaxOpenConns(25)
-	db.SetMaxIdleConns(25)
-	db.SetConnMaxLifetime(5 * time.Minute)
 
 	err = db.Ping()
 	if err != nil {
