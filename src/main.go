fe84de17c49ec8e9d815bfee9e6f3726dac954a4
Francisco Lisboa <francisco.lisboa@poatek.com>
2024-11-24 22:27:38 -0300

    fixed running local, used goose for migrations

--- src/main.go
+++ src/main.go
@@ -4,12 +4,31 @@ import (
 	"const/application/api"
 	"const/infrastructure/database"
 	"log"
+
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db := database.StartDB()
 	defer db.Close()
 
-	log.Println("Starting application on AWS Lambda...")
-	api.Init(db)
+	if os.Getenv("RUN_MODE") == "local" {
+
+		gin.Default()
+
+		myApp := api.NewApp(db)
+
+		port := os.Getenv("API_PORT")
+		if port == "" {
+			port = "8080"
+		}
+
+		api.RunServer(port, myApp)
+
+	} else {
+		log.Println("Starting application on AWS Lambda...")
+		api.Init(db)
+	}
 }
